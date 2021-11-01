69b6a696dfa02cdcb29171bbb36f626fffaff42f
kqrse <xkarsonnguyen@gmail.com>
2021-11-01 15:02:18 -0700

    monolith is now destructable

--- Legacy of the Rift/Assets/Scripts/Enemies/EnemyWeapon.cs
+++ Legacy of the Rift/Assets/Scripts/Enemies/EnemyWeapon.cs
@@ -9,15 +9,22 @@ public class EnemyWeapon : MonoBehaviour
     public int attackDamage = 10;
     public float attackRange = 0.5f;
     public LayerMask playerLayer;
+    public LayerMask monolithLayer;
 
     public Transform attackPoint;
 
     public void Attack()
     {
-        Collider2D colInfo = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
-        if (colInfo != null)
+        Collider2D playerColInfo = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
+        if (playerColInfo != null)
         {
             FindObjectOfType<PlayerHealth>().TakeDamage(attackDamage);
+        }
+        
+        Collider2D monolithColInfo = Physics2D.OverlapCircle(attackPoint.position, attackRange, monolithLayer);
+        if (monolithColInfo != null)
+        {
+            FindObjectOfType<LegendaryMonolith>().TakeDamage(attackDamage);
         }
     }
 
