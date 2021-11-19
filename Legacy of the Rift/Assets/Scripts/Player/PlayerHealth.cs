ea2f01ccb1209a3d4902daafd4189e344a44c6a6
erikawawawa <67659062+erikawawawa@users.noreply.github.com>
2021-11-18 16:21:15 -0800

    skeleton enemies plus lokir ouch

--- Legacy of the Rift/Assets/Scripts/Player/PlayerHealth.cs
+++ Legacy of the Rift/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,8 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public float currentHealth;
+    private PlayerSwitching playerSwitchScript;
+    public Animator animator;
 
     public HealthBar healthBar;
 
@@ -17,12 +19,14 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        //playerSwitchScript = GetComponent<PlayerSwitching>();
+        
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-
+        //animator.SetTrigger("isOuch");
         healthBar.SetHealth(currentHealth);
         //FindObjectOfType<AudioManager>().Play("BjornHit");
 
