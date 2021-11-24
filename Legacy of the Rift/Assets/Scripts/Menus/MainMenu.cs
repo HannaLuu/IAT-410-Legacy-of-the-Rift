b64ed29eb111b520745047ff5d2450210ee625fb
HannaLuu <pokemonhanna@gmail.com>
2021-11-23 20:18:32 -0800

    Beta Demo Build

--- Legacy of the Rift/Assets/Scripts/Menus/MainMenu.cs
+++ Legacy of the Rift/Assets/Scripts/Menus/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Intro_AfterVO");
+        SceneManager.LoadScene("Shore_Landing");
     }
 
     public void QuitGame()
