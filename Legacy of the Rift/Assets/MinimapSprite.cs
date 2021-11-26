3b78c3f3714816ccc83e3153fdd3a6f17051a08c
kqrse <xkarsonnguyen@gmail.com>
2021-11-26 01:38:33 -0800

    minimap deez nuts

---
+++ Legacy of the Rift/Assets/MinimapSprite.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapSprite : MonoBehaviour
+{
+    public GameObject parent;
+
+    void Update() {
+        var parentPos = parent.transform.position;
+        transform.position = new Vector3(parentPos.x, parentPos.y, transform.position.z);
+    }
+}
