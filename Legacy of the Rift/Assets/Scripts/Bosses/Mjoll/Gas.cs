169310ffacc6660d0bbe305f72ad768053e11849
HannaLuu <pokemonhanna@gmail.com>
2021-12-17 20:42:02 -0800

    Revert "MJOLL FIXED + KHAJIIT SACRIFICE"
    This reverts commit 7a2d392eaf1841982278e961aef7522f2ccc3131.

--- Legacy of the Rift/Assets/Scripts/Bosses/Mjoll/Gas.cs
+++ Legacy of the Rift/Assets/Scripts/Bosses/Mjoll/Gas.cs
@@ -6,8 +6,11 @@ public class Gas : MonoBehaviour
 {
     public float damage;
 
+    public float maxLifeSpan;
+    public float currentLifeSpan;
+
     public float startDamageTimer;
-    public float damageTimer;
+    private float damageTimer;
 
     public GameObject damagePopupPrefab;
 
@@ -18,6 +21,7 @@ public class Gas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentLifeSpan = maxLifeSpan;
         damageTimer = startDamageTimer;
         isColliding = false;
     }
@@ -25,12 +29,16 @@ public class Gas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentLifeSpan -= Time.deltaTime;
+        if (currentLifeSpan <= 0)
+        {
+            Destroy(gameObject);
+        }
 
         if (isColliding && damageTimer <= 0)
         {
-            //damageTimer = startDamageTimer;
-        }
-        else
+            damageTimer = startDamageTimer;
+        } else
         {
             damageTimer -= Time.deltaTime;
         }
@@ -56,11 +64,10 @@ public class Gas : MonoBehaviour
                 DamagePopup damagePopupScript = damagePopup.GetComponent<DamagePopup>();
                 damagePopupScript.Setup(damage);
                 FindObjectOfType<PlayerHealth>().TakeDamage(damage);
-            } 
-            //else
-            //{
-            //    damageTimer -= Time.deltaTime;
-            //}
+            } else
+            {
+                damageTimer -= Time.deltaTime;
+            }
         }
     }
 
